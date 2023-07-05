ed0225db8ca6d5e8f1609fa4e6b5dc049ce6a890
LilahDim <frostbitezebra421@gmail.com>
2023-07-05 15:30:14 -0700

    added zustand store and provider started form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,17 @@
-'use client'
+"use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function SetupPage() {
-  return (
-    <div className="p-4">
-      <Modal isOpen title="Test" description="Test Desc" onClose={() => {}} >
-        Hello World
-      </Modal>
-    </div>
-  );
+  const { isOpen, onOpen} = useStoreModal();
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 }
